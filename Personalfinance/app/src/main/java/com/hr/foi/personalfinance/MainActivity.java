75542d15888a90a47f40d74435070a0916831994
dhorvat3 <dhorvat3@foi-hr>
2016-11-06 22:28:04 +0100

    Izrađena osnovna funkcionalnost manija.

--- Personalfinance/app/src/main/java/com/hr/foi/personalfinance/MainActivity.java
+++ Personalfinance/app/src/main/java/com/hr/foi/personalfinance/MainActivity.java
@@ -1,13 +1,38 @@
 package com.hr.foi.personalfinance;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.hr.foi.personalfinance.fragments.Flow;
+import com.hr.foi.personalfinance.fragments.Profile;
+import com.hr.foi.userinterface.MainMenu;
+
+public class MainActivity extends Activity {
+
+    private MainMenu menu = new MainMenu();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActionBar bar = getActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0665D6")));
+
+        menu.initFrag(Flow.newInstance(getString(R.string.frag_flow)),
+                Profile.newInstance(getString(R.string.frag_profile)));
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.navdrawer, menu);
+        ft.commit();
+    }
+
+    @Override
+    public void onBackPressed(){
+        menu.homeFragment();
     }
 }
