4802dbf3d01a15488f7e3e1c0e879c69dcd78445
dhorvat3 <dhorvat3@foi.hr>
2016-12-31 14:39:45 +0100

    Dodavanje kategorije
    Skripta za dodavanje kategorije i povezivanje s odgovarajućim
    korisnikom.

---
+++ php skripte/add_category.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* Create new category and assing it to user
+*
+* --- PARAMS ---
+* [user_id]
+* [category_title]
+* [category_description]
+**/
+$data = json_decode(file_get_contents('php://input'), true);
+$response = array();
+
+require_once __DIR__.'/db.php';
+$db = new DB_CONNECT();
+$con = $db->connect();
+
+//Add new category
+$results = array();
+$sql = "INSERT INTO category VALUES (DEFAULT, '".$data['category_title']."', '".$data['category_description']."');";
+$results = mysqli_query($con, $sql);
+
+//Get id of inserted category
+$cat_id = $con->insert_id;
+
+//assign category to user
+$results = array();
+$sql = "INSERT INTO user_category VALUES (".$data['user_id'].", ".$cat_id.", 1);";
+$results = mysqli_query($con, $sql);
+
+?>
