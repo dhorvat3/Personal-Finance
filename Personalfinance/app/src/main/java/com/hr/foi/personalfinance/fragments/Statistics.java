a848ffd216ffcd762696e72952a32a4feb2f5d49
dlackovi2 <dlackovi2@foi.hr>
2017-01-28 19:24:47 +0100

    Redizajn

--- Personalfinance/app/src/main/java/com/hr/foi/personalfinance/fragments/Statistics.java
+++ Personalfinance/app/src/main/java/com/hr/foi/personalfinance/fragments/Statistics.java
@@ -2,44 +2,101 @@ package com.hr.foi.personalfinance.fragments;
 
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import com.hr.foi.personalfinance.R;
-import com.hr.foi.personalfinance.adapter.MyListAdapter;
 import com.hr.foi.userinterface.BaseFragment;
 import com.hr.foi.userinterface.FragmentInterface;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.io.Console;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 import entities.DataBuilder;
 import entities.DataInterface;
+import lecho.lib.hellocharts.gesture.ContainerScrollType;
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.view.ColumnChartView;
+import lecho.lib.hellocharts.view.LineChartView;
+import lecho.lib.hellocharts.view.PieChartView;
+import pojo.Category_;
 import pojo.Record_;
 
 /**
  * Created by dominik on 21.12.16..
  */
 
+class CategoryC
+{
+    private String id;
+    private float prihodi;
+    private float rashodi;
+
+    public CategoryC(String id, float prihodi, float rashodi)
+    {
+        this.id = id;
+        this.prihodi = prihodi;
+        this.rashodi = rashodi;
+    }
+
+    public float getRashodi()
+    {
+        return rashodi;
+    }
+
+    public void setRashodi(float rashodi)
+    {
+        this.rashodi += rashodi;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public float getPrihodi()
+    {
+        return prihodi;
+    }
+
+    public void setPrihodi(float prihodi)
+    {
+        this.prihodi += prihodi;
+    }
+}
+
 public class Statistics extends BaseFragment implements FragmentInterface, DataInterface
 {
     private DataBuilder dataBuilder = new DataBuilder(this);
     private SharedPreferences preferences;
-    private ArrayList<Record_> records;
+    private ArrayList<Record_> records = new ArrayList<Record_>();
+    private ArrayList<Category_> categories = new ArrayList<Category_>();
+    private View view;
+    private int blue = new Color().rgb(0, 77, 153);
+    private int red = new Color().rgb(204, 0, 0);
 
-
-    public static final Statistics newInstance(String name){
+    public static final Statistics newInstance(String name)
+    {
         Statistics s = new Statistics();
         s.setName(name);
 
@@ -66,174 +123,174 @@ public class Statistics extends BaseFragment implements FragmentInterface, DataI
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        String userId = userID();
-        dataBuilder.getRecords(userId);
-
-
-
-        View view = inflater.inflate(R.layout.statistics_layout, container, false);
-
-        // Line Graph za prihode ukupno po danu u mjesecu
-        // x os dodaj da prikazuje dan u mjesecu, y je iznos i to je ok
-        GraphView lineGraphPrihodi = (GraphView) view.findViewById(R.id.lineGraphPrihodi);
-        LineGraphSeries<DataPoint> lineGraphSeries1p = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 8),
-                new DataPoint(4, 1),
-                new DataPoint(5, 5),
-                new DataPoint(6, 9),
-                new DataPoint(7, 2),
-                new DataPoint(8, 1),
-                new DataPoint(9, 4),
-                new DataPoint(10, 3),
-                new DataPoint(11, 2),
-                new DataPoint(12, 7),
-                new DataPoint(13, 3),
-                new DataPoint(14, 2),
-                new DataPoint(15, 1),
-                new DataPoint(16, 5),
-                new DataPoint(17, 3),
-                new DataPoint(18, 8),
-                new DataPoint(19, 1),
-                new DataPoint(20, 5),
-                new DataPoint(21, 9),
-                new DataPoint(22, 2),
-                new DataPoint(23, 1),
-                new DataPoint(24, 4),
-                new DataPoint(25, 3),
-                new DataPoint(26, 2),
-                new DataPoint(27, 7),
-                new DataPoint(28, 3),
-                new DataPoint(29, 2),
-                new DataPoint(30, 2),
-                new DataPoint(31, 5),
-                new DataPoint(32, 3),
-                new DataPoint(33, 8),
-                new DataPoint(34, 1),
-                new DataPoint(35, 5),
-                new DataPoint(36, 9),
-                new DataPoint(37, 7),
-                new DataPoint(38, 1),
-                new DataPoint(39, 4),
-                new DataPoint(40, 8),
-                new DataPoint(41, 2),
-                new DataPoint(42, 7),
-                new DataPoint(43, 3),
-                new DataPoint(44, 2),
-                new DataPoint(45, 16)
-        });
-
-        lineGraphSeries1p.setColor(Color.GREEN);
-        lineGraphPrihodi.setTitle("Prihodi po danu");
-        lineGraphPrihodi.setTitleColor(Color.BLACK);
-        lineGraphPrihodi.setTitleTextSize(30);
-        lineGraphPrihodi.addSeries(lineGraphSeries1p);
-
-        // activate horizontal zooming and scrolling
-        lineGraphPrihodi.getViewport().setScalable(true);
-        // activate horizontal scrolling
-        lineGraphPrihodi.getViewport().setScrollable(true);
-        // activate horizontal and vertical zooming and scrolling
-        lineGraphPrihodi.getViewport().setScalableY(true);
-        // activate vertical scrolling
-        lineGraphPrihodi.getViewport().setScrollableY(true);
-
-
-        // Line Graph za rashode ukupno po danu u mjesecu
-        // x os dodaj da prikazuje dan u mjesecu, y je iznos i to je ok
-        GraphView lineGraphRashodi = (GraphView) view.findViewById(R.id.lineGraphRashodi);
-        LineGraphSeries<DataPoint> lineGraphSeries1r = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-
-        lineGraphSeries1r.setColor(Color.RED);
-        lineGraphRashodi.setTitle("Rashodi po danu");
-        lineGraphRashodi.setTitleColor(Color.BLACK);
-        lineGraphRashodi.setTitleTextSize(30);
-        lineGraphRashodi.addSeries(lineGraphSeries1r);
-
-        // activate horizontal zooming and scrolling
-        lineGraphRashodi.getViewport().setScalable(true);
-        // activate horizontal scrolling
-        lineGraphRashodi.getViewport().setScrollable(true);
-        // activate horizontal and vertical zooming and scrolling
-        lineGraphRashodi.getViewport().setScalableY(true);
-        // activate vertical scrolling
-        lineGraphRashodi.getViewport().setScrollableY(true);
-
-        // Line Graph za ukupni balance po danu u mjesecu
-        // x os dodaj da prikazuje dan u mjesecu, y je iznos i to je ok
-        GraphView lineGraph2 = (GraphView) view.findViewById(R.id.lineGraph2);
-        LineGraphSeries<DataPoint> lineGraphSeries2 = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-
-        lineGraph2.setTitle("Ukupno stanje po danu");
-        lineGraph2.setTitleColor(Color.BLACK);
-        lineGraph2.setTitleTextSize(30);
-        lineGraph2.addSeries(lineGraphSeries2);
-
-        // activate horizontal zooming and scrolling
-        lineGraph2.getViewport().setScalable(true);
-        // activate horizontal scrolling
-        lineGraph2.getViewport().setScrollable(true);
-        // activate horizontal and vertical zooming and scrolling
-        lineGraph2.getViewport().setScalableY(true);
-        // activate vertical scrolling
-        lineGraph2.getViewport().setScrollableY(true);
-
-
-        // Bar Chart za kategorije po mjesecu
-        //x os dodaj da je naziv kategorije, y je iznos i ok je
-        //mozda dodati legendu
-        GraphView barChart = (GraphView) view.findViewById(R.id.barChart);
-        BarGraphSeries<DataPoint> barGraphSeries = new BarGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-
-        barGraphSeries.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-            @Override
-            public int get(DataPoint data) {
-                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
-            }
-        });
-
-        barGraphSeries.setSpacing(50);
-
-        // draw values on top
-        barGraphSeries.setDrawValuesOnTop(true);
-        barGraphSeries.setValuesOnTopColor(Color.RED);
-        //series.setValuesOnTopSize(50);
-
-        barChart.setTitle("Ukupno po kategorijama");
-        barChart.setTitleColor(Color.BLACK);
-        barChart.setTitleTextSize(30);
-        barChart.addSeries(barGraphSeries);
-
-        // activate horizontal zooming and scrolling
-        barChart.getViewport().setScalable(true);
-        // activate horizontal scrolling
-        barChart.getViewport().setScrollable(true);
-        // activate horizontal and vertical zooming and scrolling
-        barChart.getViewport().setScalableY(true);
-        // activate vertical scrolling
-        barChart.getViewport().setScrollableY(true);
-
+        view = inflater.inflate(R.layout.statistics_layout, container, false);
+        dataBuilder.getCategories(userID());
         return view;
+    }
+
+    private void setLineChart(LineChartView view, String type)
+    {
+        LineChartData data = new LineChartData();
+        int color;
+
+        switch (type)
+        {
+            case "1":
+                color = blue;
+                break;
+            case "0":
+                color = red;
+                break;
+            default:
+                color = blue;
+        }
+        Axis axisX = new Axis();
+        Axis axisY = new Axis().setHasLines(true);
+        axisX.setName("Datum").setHasTiltedLabels(true);
+        axisY.setName("Iznos");
+
+        List<AxisValue> xValues = new ArrayList<AxisValue>();
+        List<PointValue> yValues = new ArrayList<PointValue>();
+        for (int i = 0; i < records.size(); i++)
+        {
+            if(records.get(i).getVrsta().equals(type))
+            {
+                xValues.add(new AxisValue(i, records.get(i).getDatum().substring(0, 10).toCharArray()));
+                axisX.setValues(xValues);
+                yValues.add(new PointValue(i, Float.valueOf(records.get(i).getIznos())));
+            }
+        }
+
+        data.setAxisXBottom(new Axis(xValues).setHasLines(true));
+        data.setAxisXBottom(axisX);
+        data.setAxisYLeft(axisY);
+
+        Line line = new Line(yValues).setColor(color);
+        line.setHasLabelsOnlyForSelected(true);
+
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        data.setLines(lines);
+
+        view.setInteractive(true);
+        view.setZoomType(ZoomType.HORIZONTAL);
+        view.setContainerScrollEnabled(true, ContainerScrollType.HORIZONTAL);
+        view.setLineChartData(data);
+    }
+
+    private void setColumnChart(ColumnChartView view)
+    {
+        ColumnChartData data;
+        Axis axisX = new Axis();
+        Axis axisY = new Axis().setHasLines(true);
+        axisX.setName("Kategorija");
+        axisY.setName("Iznos");
+
+        int numColumns = categories.size();
+
+        List<CategoryC> tempData = new ArrayList<CategoryC>();
+        for(Category_ item : categories)
+        {
+            tempData.add(new CategoryC(item.getId(), 0, 0));
+        }
+
+        for(CategoryC item: tempData)
+        {
+            for(Record_ record : records)
+            {
+                if(record.getVrsta().equals("1"))
+                    item.setPrihodi(Float.valueOf(record.getIznos()));
+                else
+                    item.setRashodi(Float.valueOf(record.getIznos()));
+            }
+        }
+
+        List<Column> columns = new ArrayList<Column>();
+        List<SubcolumnValue> values;
+        List<AxisValue> xValues = new ArrayList<AxisValue>();
+        for (int i = 0; i < numColumns; i++)
+        {
+            values = new ArrayList<SubcolumnValue>();
+
+            values.add(new SubcolumnValue(tempData.get(i).getPrihodi(), blue));
+            values.add(new SubcolumnValue(tempData.get(i).getRashodi(), red));
+
+            xValues.add(new AxisValue(i, categories.get(i).getTitle().toCharArray()));
+            axisX.setValues(xValues);
+
+            Column column = new Column(values);
+            column.setHasLabels(true);
+            column.setHasLabelsOnlyForSelected(true);
+            columns.add(column);
+        }
+
+        data = new ColumnChartData(columns);
+        data.setAxisXBottom(new Axis(xValues).setHasLines(true));
+
+        data.setAxisXBottom(axisX);
+        data.setAxisYLeft(axisY);
+
+        view.setColumnChartData(data);
+    }
+
+    private void setPieChart(PieChartView view, TextView textview)
+    {
+        PieChartData data;
+        float sumP = 0;
+        float sumR = 0;
+        float fillRatio = 0.9f;
+
+        for(Record_ item : records)
+        {
+            if(item.getVrsta().equals("1"))
+                sumP += Float.valueOf(item.getIznos());
+            else
+                sumR += Float.valueOf(item.getIznos());
+        }
+
+        float difference = sumP - sumR;
+        if(difference >= 0.0)
+        {
+            textview.setText("+ " + String.valueOf(difference) + " HKN");
+            textview.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
+        }
+        else
+        {
+            textview.setText("- " + String.valueOf(Math.abs(difference)) + " HKN");
+            textview.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+        }
+
+        List<SliceValue> values = new ArrayList<SliceValue>();
+
+        SliceValue sliceP = new SliceValue(Math.round(sumP), blue).setLabel("Prihod (" + sumP + ")");
+        SliceValue sliceR = new SliceValue(Math.round(sumR), red).setLabel("Rashod (" + sumR + ")");
+
+        values.add(sliceP);
+        values.add(sliceR);
+
+        data = new PieChartData(values);
+        data.setHasLabels(true);
+        data.setHasLabelsOnlyForSelected(false);
+        data.setHasLabelsOutside(true);
+        data.setHasCenterCircle(true);
+
+        view.setCircleFillRatio(fillRatio);
+        view.setPieChartData(data);
+    }
+
+    private void setData()
+    {
+        LineChartView lineGraphPrihodi = (LineChartView) view.findViewById(R.id.lineGraphPrihodi);
+        LineChartView lineGraphRashodi = (LineChartView) view.findViewById(R.id.lineGraphRashodi);
+        ColumnChartView columnGraphKategorije = (ColumnChartView) view.findViewById(R.id.columnGraphKategorije);
+        PieChartView pieGraphPR = (PieChartView) view.findViewById(R.id.pieGraphPR);
+        TextView textview = (TextView) view.findViewById(R.id.balance);
+
+        setLineChart(lineGraphPrihodi, "1");
+        setLineChart(lineGraphRashodi, "0");
+        setPieChart(pieGraphPR, textview);
+        setColumnChart(columnGraphKategorije);
     }
 
     /**
@@ -246,19 +303,36 @@ public class Statistics extends BaseFragment implements FragmentInterface, DataI
      */
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
+
+        dataBuilder.getRecords(userID());
     }
 
     @Override
     public void buildData(Object data)
     {
-        pojo.Record record = (pojo.Record) data;
-        if (record !=null)
+        if(data instanceof pojo.Category)
         {
-            records = new ArrayList<Record_>();
-
-            for (Record_ item : record.getRecord())
+            pojo.Category category = (pojo.Category) data;
+            if (category !=null)
             {
-                records.add(item);
+                for (Category_ item : category.getCategory())
+                {
+                    categories.add(item);
+                }
+            }
+        }
+
+        if(data instanceof pojo.Record)
+        {
+            pojo.Record record = (pojo.Record) data;
+            if (record !=null)
+            {
+                for (Record_ item : record.getRecord())
+                {
+                    records.add(item);
+                }
+
+                setData();
             }
         }
     }
